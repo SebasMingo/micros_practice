2f09d9f127d528712fcb40ba094f56f687afa24d
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-10-30 20:12:08 -0300

    Add files via upload

--- test8.py
+++ test8.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-diccionario = {"clave1": "valor1", "clave2": "valor2", "clave3": "valor3"}
+diccionario = [{"clave1": "valor1"},{"clave2": "valor2"},{"clave3": "valor3"} ]
 
+
+#traer titulo y descripcion siendo las claves y los valores estos ultimos
 @app.route('/traer', methods=['GET'])
 def traer():
     for clave in diccionario:
-        return diccionario['clave1'], diccionario['clave2']
+        return diccionario[0]['clave1'], diccionario[1]['clave2']
         
 print(traer())
 
